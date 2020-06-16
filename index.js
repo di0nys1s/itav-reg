154aa5b2fe35533dbe3e4b0713d3fa244c933c6d
Burak Seyhan <seyhanburak9@gmail.com>
2020-06-16 16:06:14 +1000

    registration page updates

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ $('.ui.form')
             terms: 'checked'
         }
     });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#uploaded-image').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
+    }
+}
+
+$("#imgInp").change(function () {
+    readURL(this);
+});
+
+$("#check-auto").checkbox({
+    onChecked: function () {
+        console.log("Autosend email now!");
+    }
+});
+
+$("#check-manual").checkbox({
+    onChecked: function () {
+        console.log("Manually send email later!");
+    }
+});
